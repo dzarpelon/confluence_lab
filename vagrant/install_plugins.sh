e3b956533d43b352f4a378951dcf0a987b214227
Diego Zarpelon <zaroelon@outlook.com>
2020-06-04 09:09:47 +0200

    added cache and hostname scripts

---
+++ vagrant/install_plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this plugin automatically updates the hostfiles, more info on https://github.com/cogitatio/vagrant-hostsupdater
+vagrant plugin install vagrant-hostsupdater
+# this plugin will ensure vagrant don't duplicate packages, more info on https://github.com/fgrehm/vagrant-cachier
+vagrant plugin install vagrant-cachier
